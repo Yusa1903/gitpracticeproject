bac2e11f1517a13f70041e0699185cb341dc686c
Yusa1903 <mutluyusa92@gmail.com>
2021-03-02 09:59:54 -0500

    second commit

---
+++ src/package1/Class1.java
@@ -0,0 +1,12 @@
+package package1;
+
+public class Class1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello word");
+		int numOfStudents = 71;
+		System.out.println(numOfStudents);
+	}
+
+}
